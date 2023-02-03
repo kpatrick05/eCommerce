a0c6fc9d3bb6573ce23b6b3ba1bf49198e9c87da
Patrik Kereszturi <k.patrick96@gmail.com>
2023-02-03 12:56:38 +0100

    ssh agent test

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/fetch', [DatabaseController::class, 'fetchData']);
+Route::get('/post', [DatabaseController::class, 'fetchData']);
